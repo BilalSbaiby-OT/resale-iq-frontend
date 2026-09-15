6f28b7eee41e7ed981e2576737a65b4305b37748
BilalSbaiby-OT <emmanuelbilal33@gmail.com>
2026-09-15 05:32:06 +0200

    feat: POSTS_38: supreme-box-logo-tee-eu-vinted-price-guide — 154 watched departures/7d, €66 avg, Box Logo tee €60-180 by season/condition, 6 sections, 6 FAQs, AEO definedTerm, Balenciaga/Gucci comparison

--- src/data/blog-posts.ts
+++ src/data/blog-posts.ts
@@ -809,8 +809,9 @@ import { POSTS_34 } from "./blog-posts-34"
 import { POSTS_35 } from "./blog-posts-35"
 import { POSTS_36 } from "./blog-posts-36"
 import { POSTS_37 } from "./blog-posts-37"
+import { POSTS_38 } from "./blog-posts-38"
 
-export const ALL_POSTS: BlogPost[] = [...POSTS, ...POSTS_2, ...POSTS_3, ...POSTS_4, ...POSTS_5, ...POSTS_6, ...POSTS_7, ...POSTS_8, ...POSTS_9, ...POSTS_10, ...POSTS_11, ...POSTS_12, ...POSTS_13, ...POSTS_14, ...POSTS_15, ...POSTS_16, ...POSTS_17, ...POSTS_18, ...POSTS_19, ...POSTS_20, ...POSTS_21, ...POSTS_22, ...POSTS_23, ...POSTS_24, ...POSTS_25, ...POSTS_26, ...POSTS_27, ...POSTS_28, ...POSTS_29, ...POSTS_30, ...POSTS_31, ...POSTS_32, ...POSTS_33, ...POSTS_34, ...POSTS_35, ...POSTS_36, ...POSTS_37]
+export const ALL_POSTS: BlogPost[] = [...POSTS, ...POSTS_2, ...POSTS_3, ...POSTS_4, ...POSTS_5, ...POSTS_6, ...POSTS_7, ...POSTS_8, ...POSTS_9, ...POSTS_10, ...POSTS_11, ...POSTS_12, ...POSTS_13, ...POSTS_14, ...POSTS_15, ...POSTS_16, ...POSTS_17, ...POSTS_18, ...POSTS_19, ...POSTS_20, ...POSTS_21, ...POSTS_22, ...POSTS_23, ...POSTS_24, ...POSTS_25, ...POSTS_26, ...POSTS_27, ...POSTS_28, ...POSTS_29, ...POSTS_30, ...POSTS_31, ...POSTS_32, ...POSTS_33, ...POSTS_34, ...POSTS_35, ...POSTS_36, ...POSTS_37, ...POSTS_38]
 
 export function getPost(slug: string): BlogPost | undefined {
   return ALL_POSTS.find((p) => p.slug === slug)
